3ce04aef7c1089669e7b44e933ba095287d1505f
wax100 <wax100@rambler.ru>
2018-03-04 13:13:42 +0300

    Snippet TvTable as modx modifier

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 Easy table creation with new type of TV for MODX
 
+1.0.7-beta
+==============
+- Now U can use TvTable snippet as modx output modifier - [[*table:tvtable]]
+
+1.0.6-beta
+==============
+- Fix using in migx, fix JS, add CSS in file
+
 1.0.5-beta
 ==============
 - Add chunks for table, tr, th, td
--- core/components/tvtable/elements/snippets/snippet.tvtable.php
+++ core/components/tvtable/elements/snippets/snippet.tvtable.php
@@ -1,46 +1,52 @@
 <?php
-/** @var modX $modx */
-/** @var array $scriptProperties */
-$tv = (int)$tv ;
- 
-if ($tv == 0){
-    return ;
-}
-$resource = isset($id) ? $id : $modx->resource->id;
-$tvObject = $modx->getObject('modTemplateVarResource', array('tmplvarid' => $tv, 'contentid' => $resource ));
- if (!$tvObject){
-    return ;
-}
-$tvv = $tvObject->get('value');
-if (!$tvv || $tvv=='[["",""],["",""]]') return;
-
-$tvtArr=json_decode($tvv);
-
-/** @var pdoFetch $pdoFetch */
-$fqn = $modx->getOption('pdoFetch.class', null, 'pdotools.pdofetch', true);
-$path = $modx->getOption('pdofetch_class_path', null, MODX_CORE_PATH . 'components/pdotools/model/', true);
-if ($pdoClass = $modx->loadClass($fqn, $path, false, true)) {
-    $pdoFetch = new $pdoClass($modx, $scriptProperties);
-} else {
-    return false;
-}
-$pdoFetch->addTime('pdoTools loaded');
-$output = $pdoFetch->run();
-
-$fastMode = $pdoFetch->config['fastMode'];
- $is_header = 1;
-for($row=0; $row<count($tvtArr); $row++) {
-    $cells = '';
-    for($i=0; $i<count($tvtArr[$row]); $i++){
-        $tpl = $tdTpl;
-        if($is_header){
-            $tpl = $thTpl;
-             $is_header=0;
-        } 
-        $cells.=$pdoFetch->getChunk($tpl,  array('val' => $tvtArr[$row][$i]), $fastMode);
-       
+    /** @var modX $modx */
+    /** @var array $scriptProperties */
+    $tv = (int)$tv ;
+    
+    if ($tv == 0  && $input == ''){
+        return ;
     }
-    $rows.=$pdoFetch->getChunk($trTpl,  array('cells' => $cells), $fastMode);
- }
-$output = $pdoFetch->getChunk($wrapperTpl,  array('table' => $rows, 'classname'=>$classname), $fastMode);
+    if($input == ''){
+        $resource = isset($id) ? $id : $modx->resource->id;
+        $tvObject = $modx->getObject('modTemplateVarResource', array('tmplvarid' => $tv, 'contentid' => $resource ));
+        if (!$tvObject){
+            return ;
+        }
+        
+        $tvv = $tvObject->get('value');
+    }
+    else{
+        $tvv = $input;
+    }
+    if (!$tvv || $tvv=='[["",""],["",""]]') return;
+    
+    $tvtArr=json_decode($tvv);
+    
+    /** @var pdoFetch $pdoFetch */
+    $fqn = $modx->getOption('pdoFetch.class', null, 'pdotools.pdofetch', true);
+    $path = $modx->getOption('pdofetch_class_path', null, MODX_CORE_PATH . 'components/pdotools/model/', true);
+    if ($pdoClass = $modx->loadClass($fqn, $path, false, true)) {
+        $pdoFetch = new $pdoClass($modx, $scriptProperties);
+        } else {
+        return false;
+    }
+    $pdoFetch->addTime('pdoTools loaded');
+    $output = $pdoFetch->run();
+    
+    $fastMode = $pdoFetch->config['fastMode'];
+    $is_header = 1;
+    for($row=0; $row<count($tvtArr); $row++) {
+        $cells = '';
+        for($i=0; $i<count($tvtArr[$row]); $i++){
+            $tpl = $tdTpl;
+            if($is_header){
+                $tpl = $thTpl;
+                $is_header=0;
+            } 
+            $cells.=$pdoFetch->getChunk($tpl,  array('val' => $tvtArr[$row][$i]), $fastMode);
+            
+        }
+        $rows.=$pdoFetch->getChunk($trTpl,  array('cells' => $cells), $fastMode);
+    }
+    $output = $pdoFetch->getChunk($wrapperTpl,  array('table' => $rows, 'classname'=>$classname), $fastMode);
 return $output;
