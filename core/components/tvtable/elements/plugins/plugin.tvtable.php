b0bb6f9d9d50b9fd01fb73d265fd1efc3426ae92
GulomovCreative <gulomovcreative@gmail.com>
2019-03-12 05:26:01 +0300

    Fix indents in main plugin

--- core/components/tvtable/elements/plugins/plugin.tvtable.php
+++ core/components/tvtable/elements/plugins/plugin.tvtable.php
@@ -3,17 +3,17 @@
 $corePath = $modx->getOption('tvtable.core_path', null, $modx->getOption('core_path') . 'components/tvtable/');
 
 switch ($modx->event->name) {
-	case 'OnTVInputRenderList':
-		$modx->event->output($corePath . 'elements/tv/input/');
-		break;
-	case 'OnTVInputPropertiesList':
-		$modx->event->output($corePath . 'elements/tv/inputoptions/');
-		break;
-	case 'OnDocFormPrerender':
+    case 'OnTVInputRenderList':
+        $modx->event->output($corePath . 'elements/tv/input/');
+        break;
+    case 'OnTVInputPropertiesList':
+        $modx->event->output($corePath . 'elements/tv/inputoptions/');
+        break;
+    case 'OnDocFormPrerender':
         $modx->regClientCSS('/assets/components/tvtable/css/mgr/tvtable.css');
-		$modx->regClientStartupScript('/assets/components/tvtable/js/mgr/tvtable.js');
-		break;
-	 case 'OnManagerPageBeforeRender':
+        $modx->regClientStartupScript('/assets/components/tvtable/js/mgr/tvtable.js');
+        break;
+    case 'OnManagerPageBeforeRender':
         $modx->controller->addLexiconTopic('tvtable:default');
-         break;
+        break;
 }
