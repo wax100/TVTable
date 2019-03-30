bf52f82e9e841dd8d7d80b2542022c8e7f39629c
GulomovCreative <gulomovcreative@gmail.com>
2019-03-30 03:02:55 +0300

    Added area lexicon

--- core/components/tvtable/lexicon/en/setting.inc.php
+++ core/components/tvtable/lexicon/en/setting.inc.php
@@ -1,4 +1,5 @@
 <?php
 
+$_lang['area_tvtable_main'] = 'Main settings';
 $_lang['setting_tvtable_clear_button'] = 'Show clearing table button';
 $_lang['setting_tvtable_clear_button_desc'] = 'Display the clearing table button.';
