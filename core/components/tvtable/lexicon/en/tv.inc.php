c99a62fdff2f093dbce4d153192165682daaa080
Bakhtovar Gulomov <gulomovcreative@gmail.com>
2019-05-22 04:34:25 +0700

    Added TV properties lexicon entries

---
+++ core/components/tvtable/lexicon/en/tv.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$_lang['tvtable.columns'] = 'Number of columns';
+$_lang['tvtable.columns_desc'] = '<em>Note: Has priority over the <strong>Maximum number of columns</strong> property. Existing values will not be changed.</em>';
+$_lang['tvtable.max_columns'] = 'Maximum number of columns';
+$_lang['tvtable.max_columns_desc'] = 'Existing values will not be changed.</em>';
+$_lang['tvtable.max_rows'] = 'Maximum number of rows';
+$_lang['tvtable.max_rows_desc'] = 'Existing values will not be changed.</em>';
