71d2cac67960bc2b38f0ede0b9917e031f8687d0
kondakovdm <scauser52@gmail.com>
2019-06-18 17:38:02 +0500

    Исправлен путь к assets

--- core/components/tvtable/elements/plugins/plugin.tvtable.php
+++ core/components/tvtable/elements/plugins/plugin.tvtable.php
@@ -1,6 +1,7 @@
 <?php
 /** @var modX $modx */
 $corePath = $modx->getOption('tvtable.core_path', null, $modx->getOption('core_path') . 'components/tvtable/');
+$assetsPath = $modx->getOption('assets_url', null, MODX_ASSETS_URL) . 'components/tvtable/';
 
 switch ($modx->event->name) {
     case 'OnTVInputRenderList':
@@ -10,8 +11,8 @@ switch ($modx->event->name) {
         $modx->event->output($corePath . 'elements/tv/inputoptions/');
         break;
     case 'OnDocFormPrerender':
-        $modx->regClientCSS('/assets/components/tvtable/css/mgr/tvtable.css?ver=3.4.1');
-        $modx->regClientStartupScript('/assets/components/tvtable/js/mgr/tvtable.js?ver=3.4.1');
+        $modx->regClientCSS($assetsPath . 'css/mgr/tvtable.css?ver=3.3.3');
+        $modx->regClientStartupScript($assetsPath . 'js/mgr/tvtable.js?ver=3.3.3');
         break;
     case 'OnManagerPageBeforeRender':
         $modx->controller->addLexiconTopic('tvtable:default');
