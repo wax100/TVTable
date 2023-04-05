766831dda20e48ea0db54989b1530dcc03ad84af
wax100 <wax100@gmail.com>
2023-04-05 14:25:04 +0300

    v4.0.0

--- core/components/tvtable/elements/snippets/snippet.tvtable.php
+++ core/components/tvtable/elements/snippets/snippet.tvtable.php
@@ -4,7 +4,7 @@
 
 $tv = $modx->getOption('tv', $scriptProperties, '');
 $input = $modx->getOption('input', $scriptProperties, '');
-$resource = (int) $modx->getOption('id', $scriptProperties, '');
+$resource = (int)$modx->getOption('id', $scriptProperties, '');
 $x = $modx->getOption('getX', $scriptProperties, '');
 $y = $modx->getOption('getY', $scriptProperties, '');
 $head = $modx->getOption('head', $scriptProperties, true, true);
@@ -25,9 +25,12 @@ $bodyOpen = empty($bodyClass) ? '<tbody>' : '<tbody class="' . $bodyClass . '">'
 
 if (empty($tv) && empty($input)) return;
 
+$version = $modx->getVersionData();
+$modx_version = intval(substr($version['full_version'], 0, 1));
+
 if (empty($input)) {
     if (!empty($resource)) {
-        $resource = $modx->getObject('modResource', array('id' => $resource));
+        $resource = $modx->getObject('modResource', ['id' => $resource]);
         if (!$resource instanceof modResource) return;
         $value = $resource->getTVValue($tv);
     } else {
@@ -42,7 +45,7 @@ if (!$tvtArr = $modx->fromJSON($value)) { return; };
 if ($x == 'first') $x = 0;
 if ($y == 'first') $y = 0;
 
-$values = array();
+$values = [];
 
 if ($x !== '' && $y === '') {
     $directionX = true;
@@ -55,7 +58,7 @@ if ($x !== '' && $y === '') {
     $directionY = true;
     if (count($tvtArr[0]) > $y) {
         foreach ($tvtArr as $key => $row) {
-            if ($y === 'last') { $y = count($tvtArr[$key]) - 1; }
+            if ($y === 'last') { $y = count($tvtArr[$key]) - 1;  }
             $values[$key] = $tvtArr[$key][$y];
         }
     }
@@ -69,15 +72,15 @@ if ($x !== '' && $y === '') {
 
 if ($display_headers && count($values)) {
     $query = $modx->newQuery('modTemplateVar');
-    $query->where(array('id' => $tv));
-    $query->where(array('name' => $tv), xPDOQuery::SQL_OR);
+    $query->where(['id' => $tv]);
+    $query->where(['name' => $tv], xPDOQuery::SQL_OR);
     if ($tv_obj = $modx->getObject('modTemplateVar', $query)) {
         $tv_props = $tv_obj->get('input_properties');
         $headers = explode('||', $tv_props['headers']);
         $headersDefault = $tv_props['headers_default'];
         if (count($headers)) {
             $column_count = ($directionX || $directionY) ? count($values) : count($values[0]);
-            $header_row = array();
+            $header_row = [];
             if ($directionY) {
                 $header_row = $headers[$y] ?: $headersDefault;
             } else {
@@ -87,7 +90,7 @@ if ($display_headers && count($values)) {
             }
             if ($directionX) {
                 $directionX = false;
-                $values = array($header_row, $values);
+                $values = [$header_row, $values];
             } else {
                 array_unshift($values, $header_row);
             }
@@ -98,24 +101,31 @@ if ($display_headers && count($values)) {
 if (empty($values)) return;
 
 /** @var pdoFetch $pdoFetch */
-$fqn = $modx->getOption('pdoFetch.class', null, 'pdotools.pdofetch', true);
-$path = $modx->getOption('pdofetch_class_path', null, MODX_CORE_PATH . 'components/pdotools/model/', true);
-if ($pdoClass = $modx->loadClass($fqn, $path, false, true)) {
-    $pdoFetch = new $pdoClass($modx, $scriptProperties);
+if ($modx_version === 3) {
+    $pdoFetch = $modx->services->get(ModxPro\PdoTools\Fetch::class);
+    if (!$pdoFetch) { return false; }
+    $fastMode = $pdoFetch->getConfig['fastMode'];
 } else {
-    return false;
+    $fqn = $modx->getOption('pdoFetch.class', null, 'pdotools.pdofetch', true);
+    $path = $modx->getOption('pdofetch_class_path', null, MODX_CORE_PATH . 'components/pdotools/model/', true);
+    if ($pdoClass = $modx->loadClass($fqn, $path, false, true)) {
+        $pdoFetch = new $pdoClass($modx, $scriptProperties);
+        $fastMode = $pdoFetch->config['fastMode'];
+    } else {
+        return false;
+    }
 }
 $pdoFetch->addTime('pdoTools loaded');
 $output = $pdoFetch->run();
-$fastMode = $pdoFetch->config['fastMode'];
+$rows = '';
 
 if ($directionX) {
     $directionXTpl = $head ? $thTpl : $tdTpl;
     foreach ($values as $value) {
-        $cells .= $pdoFetch->getChunk($directionXTpl, array('val' => $value), $fastMode);
+        $cells .= $pdoFetch->getChunk($directionXTpl, ['val' => $value], $fastMode);
     }
     $rows .= $head ? $headOpen : $bodyOpen;
-    $rows .= $pdoFetch->getChunk($trTpl, array('cells' => $cells, 'idx' => $x), $fastMode);
+    $rows .= $pdoFetch->getChunk($trTpl, ['cells' => $cells, 'idx' => $x], $fastMode);
     $rows .= $head ? '</thead>' : '</tbody>';
 } else {
     if ($head) {
@@ -129,9 +139,9 @@ if ($directionX) {
                 $col++;
             }
         } else {
-            $headCells .= $pdoFetch->getChunk($thTpl, array('val' => $head), $fastMode);
+            $headCells .= $pdoFetch->getChunk($thTpl, ['val' => $head], $fastMode);
         }
-        $rows .= $pdoFetch->getChunk($trTpl, array('cells' => $headCells, 'idx' => 0), $fastMode);
+        $rows .= $pdoFetch->getChunk($trTpl, ['cells' => $headCells, 'idx' => 0], $fastMode);
         $rows .= '</thead>';
     }
     if ($values) {
@@ -142,19 +152,19 @@ if ($directionX) {
             if(is_array($row)) {
                 $col = 0;
                 foreach ($row as $cell) {
-                    $cells .= $pdoFetch->getChunk($tdTpl, array('val' => $cell, 'row' => $idx + 1, 'col' => $col + 1), $fastMode);
+                    $cells .= $pdoFetch->getChunk($tdTpl, ['val' => $cell], $fastMode);
                     $col++;
                 }
             } else {
-                $cells .= $pdoFetch->getChunk($tdTpl, array('val' => $row), $fastMode);
+                $cells .= $pdoFetch->getChunk($tdTpl, ['val' => $row], $fastMode);
             }
-            $rows .= $pdoFetch->getChunk($trTpl, array('cells' => $cells, 'idx' => $idx), $fastMode);
+            $rows .= $pdoFetch->getChunk($trTpl, ['cells' => $cells, 'idx' => $idx], $fastMode);
             $idx++;
         }
         $rows .= '</tbody>';
     }
 }
 
-$output = $pdoFetch->getChunk($wrapperTpl, array('table' => $rows, 'classname' => $classname), $fastMode);
+$output = $pdoFetch->getChunk($wrapperTpl, ['table' => $rows, 'classname' => $classname], $fastMode);
 
 return $output;
